b9dee26cdbe4c12c3cc1577d072957e67dedb5ba
sndbox <sndbox@users.noreply.github.com>
2014-08-09 01:47:53 +0900

    Write printable ascii sequence instead of 'a'
    Also parameterized send delay, port number

--- chunked_responder/main.go
+++ chunked_responder/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -35,11 +37,60 @@ func sizeToInt(s string) (int, error) {
 	return sz * m, nil
 }
 
+type asciiChunk struct {
+	w           io.Writer
+	totalLength int
+	wroteSoFar  int
+	nextAscii   byte
+	posInBuf    int
+	buf         [4096]byte
+}
+
+func newAsciiChunk(w io.Writer, totalLength int) *asciiChunk {
+	c := &asciiChunk{w, totalLength, 0, 0, 0, [4096]byte{}}
+	c.prepareBuf()
+	return c
+}
+
+func (c *asciiChunk) prepareBuf() {
+	for i := 0; i < len(c.buf); i++ {
+		for {
+			c.nextAscii = (c.nextAscii + 1) % 128
+			if strconv.IsPrint(rune(c.nextAscii)) && c.nextAscii != '\n' {
+				break
+			}
+		}
+		c.buf[i] = c.nextAscii
+	}
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+// Writes a chunk of printable []byte, returns the number of byte written.
+func (c *asciiChunk) writeNext() int {
+	if c.wroteSoFar >= c.totalLength {
+		return 0
+	}
+	last := min(c.totalLength-c.wroteSoFar, len(c.buf)-c.posInBuf)
+	n, err := c.w.Write(c.buf[c.posInBuf:last])
+	if err != nil {
+		c.wroteSoFar = c.totalLength
+		return 0
+	}
+	c.wroteSoFar += n
+	c.posInBuf = (c.posInBuf + n) % len(c.buf)
+	return n
+}
+
 type chunkResponder struct{}
 
-func getSize(u *url.URL) (int, error) {
-	query := u.Query()
-	if size := query.Get("size"); size != "" {
+func getSize(vs url.Values) (int, error) {
+	if size := vs.Get("size"); size != "" {
 		sz, err := sizeToInt(size)
 		if err != nil {
 			return 0, err
@@ -47,6 +98,17 @@ func getSize(u *url.URL) (int, error) {
 		return sz, nil
 	}
 	return 0, fmt.Errorf("no size parameter")
+}
+
+func getDelay(vs url.Values) int {
+	if ds := vs.Get("delay"); ds != "" {
+		delay, err := strconv.Atoi(ds)
+		if err != nil {
+			return 0
+		}
+		return delay
+	}
+	return 0
 }
 
 func (c chunkResponder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -59,24 +121,32 @@ func (c chunkResponder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	sz, err := getSize(r.URL)
+	query := r.URL.Query()
+	sz, err := getSize(query)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	delay := getDelay(query)
 
-	w.Header()["Content-Type"] = []string{"text/plain"}
+	w.Header().Add("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
-	for i := 0; i < sz-1; i++ {
-		w.Write([]byte("a"))
+
+	chunk := newAsciiChunk(w, sz-1)
+	for n := chunk.writeNext(); n > 0; n = chunk.writeNext() {
 		flusher.Flush()
-		time.Sleep(time.Duration(10) * time.Millisecond)
+		if delay > 0 {
+			time.Sleep(time.Duration(delay) * time.Millisecond)
+		}
 	}
 	w.Write([]byte("\n"))
 	flusher.Flush()
 }
 
+var port = flag.String("port", "9100", "port number")
+
 func main() {
+	flag.Parse()
 	http.Handle("/", &chunkResponder{})
-	http.ListenAndServe(":9100", nil)
+	http.ListenAndServe(":"+*port, nil)
 }
